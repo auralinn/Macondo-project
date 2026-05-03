9dff4a82f1b6ae090e5c30f52f140a4e0ad80e1b
auralinn <chibuikechukwu176@gmail.com>
2026-05-03 15:41:49 +0200

    Add files via upload

---
+++ recipeweb.js
@@ -0,0 +1,20 @@
+// const searchMeal = async(e) => {
+//     e.preventDefault();
+
+//     const input = document.querySelector('.search-bar')
+//     const title = document.querySelector('.search-bar')
+//     const info = document.querySelector('.search-bar')
+//     const img = document.querySelector('.search-bar')
+//      const input = document.querySelector('.search-bar')
+
+//      const fetchMealData = async (val) =>{
+//        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${val}`)
+//      }
+// }
+
+
+// const form = document.querySelector('form')
+// form.addEventListener("submit", searchMeal);
+
+// const searchButton = document.querySelector('.maginify-glass')
+// searchButton.addEventListener("click", searchMeal);
